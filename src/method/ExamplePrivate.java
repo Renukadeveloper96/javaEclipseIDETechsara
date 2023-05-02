e8a5c29cdf886e75a4314e81a4c101c0c7965557
Renukadeveloper96 <renukamaradagi@gmail.com>
2023-05-02 20:32:56 +0530

    variables

--- src/method/ExamplePrivate.java
+++ src/method/ExamplePrivate.java
@@ -1,13 +1,17 @@
 package method;
 
-public class ExamplePrivate {
+public class ExamplePrivate{
 
 	protected void print() {
 		System.out.println("private ");
+	}
+	protected void Print() {
+		System.out.println("private HI ");
 	}
 	
 	public static void main(String[] args) {
 		ExamplePrivate ExamplePrivate=new ExamplePrivate();
 		ExamplePrivate.print();
+		ExamplePrivate.Print();
 	}
 }
---
+++ src/variable/ExampleofVariable.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class ExampleofVariable {
+	
+	static int m=100;//static variable
+	
+
+	void method() {
+	int n=90;//local variable
+	}
+	
+
+	public static void main(String[] args) {
+		int n=90;
+
+	}
+
+}
